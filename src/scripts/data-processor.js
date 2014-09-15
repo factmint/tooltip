3b97493efa9c0da27c3891c2d12316a2d2cf3b87
DanElliottPalmer <danelliottpalmer@gmail.com>
2014-09-15 10:27:23 +0100

    Remove: second argument in parseFloat as unneeded Add: getRange to DataSet which gets and caches the range

--- src/scripts/data-processor.js
+++ src/scripts/data-processor.js
@@ -5,6 +5,60 @@ define(function() {
     },
     tableToJSON: function( container ) {
 
+
+      function DataSet(){
+
+        this._cacheRange = null;
+
+        this.keys = null;
+        this.rows = null;
+
+      }
+      DataSet.prototype = {
+
+        "constructor": DataSet,
+
+        "getRange": function( key ){
+
+          var index = -1;
+
+          if( typeof key === "string" ){
+            index = this.keys[ key ];
+          } else {
+            index = key;
+          }
+
+          // Check the cache
+          if( !this._cacheRange[ index ] ){
+
+            var min = Number.MAX_VALUE,
+                max = Number.MIN_VALUE,
+                value = 0;
+
+            this.rows.forEach(function( row ){
+
+              value = row[ index ];
+
+              if( value < min ){ min = value; }
+              if( value > max ){ max = value; }
+
+            });
+
+            this._cacheRange[ index ] = {
+              "max": max,
+              "min": min
+            };
+
+          }
+
+          return this._cacheRange[ index ];
+
+        }
+
+      };
+
+
+
       var keys, i = 0, l, ii = -1, ll,
           ds = new DataSet(),
           value;
@@ -19,27 +73,20 @@ define(function() {
       l = container.rows.length;
       // Minus the column header
       ds.rows = new Array( l - 1 );
+      ds._cacheRange = new Array( l - 1 );
       while( ++i < l ){
         ds.rows[ i - 1 ] = new Array( ll );
         ii = -1;
         while( ++ii < ll ){
           value = container.rows[i].cells[ii].textContent;
           if( !isNaN( value ) ){
-            value = parseFloat( value, 10 );
+            value = parseFloat( value );
           }
           ds.rows[ i - 1 ][ ii ] = value;
         }
       }
 
       return ds;
-
-      function DataSet(){
-        this.keys = null;
-        this.rows = null;
-      }
-      DataSet.prototype = {
-        "constructor": DataSet
-      };
 
     }
   }
